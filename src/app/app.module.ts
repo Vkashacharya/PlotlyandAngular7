ec106c4af1ac0634138915e226a2a90ab6d3b4e4
Gritfeat <vkash.acharya03@gmail.com>
2019-02-12 17:48:03 +0545

    initialcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PredictionComponent } from './prediction/prediction.component';
+import { PlotlyModule } from 'angular-plotly.js';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PredictionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,PlotlyModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
